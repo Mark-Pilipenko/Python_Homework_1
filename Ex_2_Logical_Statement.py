130e697b758c0280e4663a23d01dd8eb96e9be89
Mark <kalmark@mail.ru>
2022-12-19 22:52:27 +0300

    Fix and optimize

--- Ex_2_Logical_Statement.py
+++ Ex_2_Logical_Statement.py
@@ -1,13 +1,8 @@
 # Напишите программу для. проверки истинности утверждения 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-def CheckTrue(x, y, z):
-    if not (x or y or z) == (not x) and (not y) and (not z):
-        return 'True'
-    return 'False'
-
 print('Проверка истинности утверждения ¬(X v Y v Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат')
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            print(f'При (X = {x}, Y = {y}, Z = {z}) - {CheckTrue(x, y, z)}')
+            print(f'При (X = {x}, Y = {y}, Z = {z}) - {not (x or y or z) == ((not x) and (not y) and (not z))}')
